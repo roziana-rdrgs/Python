1c391070b11e9db22a9e3c4ffeae0bfa9711e859
Roziana Rodrigues <pereiraroziana55@gmail.com>
2019-08-13 10:56:35 -0300

    03
    -

--- Teste/cinco.py
+++ Teste/cinco.py
@@ -3,9 +3,9 @@ peso = float(input("Peso: "))
 sexo = input("Sexo: ")
 peso_ideal = 0
 
-if sexo == "F" :
+if sexo == "F":
     peso_ideal = (62.1 * altura)-44.7
-elif sexo == "M" :
+elif sexo == "M":
     peso_ideal = (72.7 * altura)-58
 else:
     print("Informação inválida.")
